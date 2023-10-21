72bebb28415d2e168559e255835f01525be84888
duradurian <113811889+duradurian@users.noreply.github.com>
2023-10-20 23:42:21 -0400

    revisions

--- virtual_mouse_dual_buffer.py
+++ virtual_mouse_dual_buffer.py
@@ -1,8 +1,7 @@
 '''
-NOTE: RECOMMENDED THAT WEBCAM AND MONITOR IS 1920x1080 or a multiple or 2 or 1.5.
-UPDATE NOTE: ADDED ONE FRAME BUFFER TO INCREASE SMOOTHENING
+NOTE: RECOMMENDED THAT WEBCAM IS 1920x1080. MANUALLY SET SCREEN RES!
+UPDATE NOTE: ADDED AVERAGING BUFFER TO INCREASE SMOOTHENING AND INCREASE HAND PRECISION
 '''
-
 import cv2
 import mediapipe as mp
 import pyautogui
@@ -10,28 +9,27 @@ import asyncio
 import math
 import time
 
-# global constants 
-global_scale = 1
+# global constants
+# screen_width, screen_height = pyautogui.size()
+global_scale = 4
+window_start_scale = 2
 screen_width = 1920
 screen_height = 1080
 show_window = False
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
 
 def midpoint(point_x, point_y, point_2x, point_2y):
     return int((point_x+point_2x)//2),int((point_y+point_2y)//2)
 
 async def main():
-    # grabs screen resolution
-    # screen_width, screen_height = pyautogui.size()
     print(f"screen width, height: {screen_width}, {screen_height}")
 
     print("Loading CV2...")
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(1920/global_scale))
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(1080/global_scale))
     print("Done!")
-
-    pyautogui.PAUSE = 0
-    pyautogui.FAILSAFE = False
 
     if not cap.isOpened():
         print("Error: Could not open camera.")
@@ -83,13 +81,13 @@ async def main():
                     y = int(landmark.y * frame_height)
                     
                     if id == 8:
-                        cv2.circle(frame, center=(x, y), radius=30, color=(0, 255, 0)) # green pointer
+                        cv2.circle(frame, center=(x, y), radius=30//global_scale, color=(0, 255, 0)) # green pointer
                         
                         index_x = screen_width / frame_width * x
                         index_y = screen_height / frame_height * y
 
                     if id == 4: 
-                        cv2.circle(frame, center=(x, y), radius=10, color=(0, 0, 255)) # smaller red pointer
+                        cv2.circle(frame, center=(x, y), radius=10//global_scale, color=(0, 0, 255)) # smaller red pointer
                         
                         thumb_x = screen_width / frame_width * x
                         thumb_y = screen_height / frame_height * y
@@ -116,7 +114,7 @@ async def main():
                         index_thumb_distance = math.sqrt(abs(index_y-thumb_y)**2+abs(index_x-thumb_x)**2)
                         
                         try:
-                            if index_thumb_distance < 60 and index_thumb_distance > 14: # optimized using histogram pattern
+                            if index_thumb_distance < 60 and index_thumb_distance > 14:
                                 pyautogui.mouseDown()
                                 status_message = "left click!"
                                 print(F"click dist: {index_thumb_distance}")
@@ -125,7 +123,7 @@ async def main():
                         except:
                             print('error: attempted to click')
 
-            if show_window:               
+            if show_window and global_scale == 1:
                 # draws line between index and thumb, adds distance on midpoint of line
                 cv2.line(frame, (int(index_x), int(index_y)), (int(thumb_x), int(thumb_y)), (255, 0, 0), 3)
                 line_midpoint = midpoint(index_x,index_y,thumb_x,thumb_y)
@@ -139,14 +137,18 @@ async def main():
         
         if show_window:
             # Status indicators in THIS ORDER: FPS, Index cord, thumb cord, click stat
-            cv2.putText(frame, f"Index finger: {index_x}, {index_y}", (int(50), int(50)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2, cv2.LINE_AA)
-            cv2.putText(frame, f"Thumb finger: {thumb_x}, {thumb_y}", (int(50), int(90)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2, cv2.LINE_AA)
-            cv2.putText(frame, f"Click status: {status_message}", (int(50), int(130)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2, cv2.LINE_AA)
-            cv2.putText(frame, f"fps: {int(fps)}", (50, 180), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 2, cv2.LINE_AA)
+            cv2.putText(frame, f"Index finger: {index_x}, {index_y}", (int(50/global_scale), int(50/global_scale)), cv2.FONT_HERSHEY_SIMPLEX, 1.3/global_scale, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(frame, f"Thumb finger: {thumb_x}, {thumb_y}", (int(50/global_scale), int(90/global_scale)), cv2.FONT_HERSHEY_SIMPLEX, 1.3/global_scale, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(frame, f"Click status: {status_message}", (int(50/global_scale), int(130/global_scale)), cv2.FONT_HERSHEY_SIMPLEX, 1.3/global_scale, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(frame, f"fps: {int(fps)}", (int(50//global_scale), int(180/global_scale)), cv2.FONT_HERSHEY_PLAIN, 3/global_scale, (0,0,255), 1, cv2.LINE_AA)
             
             # final output!!!
             cv2.namedWindow("Virtual Mouse", cv2.WINDOW_NORMAL)
-            cv2.resizeWindow("Virtual Mouse", int(1920/global_scale), int(1080/global_scale))
+
+            if frame_number == 0:
+                cv2.resizeWindow("Virtual Mouse", int(screen_width/window_start_scale), int(screen_height/window_start_scale))
+                print('Scaled CV2 Window Size!')
+
             cv2.imshow('Virtual Mouse', frame)
 
         frame_number += 1
