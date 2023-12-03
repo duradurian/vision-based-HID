8ac7a3425a4dc48200793128c709d43122371548
duradurian <113811889+duradurian@users.noreply.github.com>
2023-12-02 22:47:12 -0500

    Update virtual_mouse_dual_buffer.py

--- virtual_mouse_dual_buffer.py
+++ virtual_mouse_dual_buffer.py
@@ -22,15 +22,16 @@ screen_width = 1920
 screen_height = 1080
 show_window = True
 click_mechanic_aceleration_threshold = 1.6
-WINDOW_NAME = "Virtual Mouse: q(exit), w(hide/show stats), e(zero dist. dragging)"
+WINDOW_NAME = "Virtual Mouse: q(exit), w(hide/show stats), e(zero dist. dragging), r(RGB 4on/off)"
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = False
 click_distance = 120
 
 # global variables
 click_status = 0
-zero_distance_dragging = True
+zero_distance_dragging = False
 show_stats = True
+rgb_stat = True
 
 def generate_spectrum_color(color, speed):
     # Convert color to HSV
@@ -47,16 +48,16 @@ def generate_spectrum_color(color, speed):
 def midpoint(point_x, point_y, point_2x, point_2y):
     return int((point_x+point_2x)//2),int((point_y+point_2y)//2)
 
-def click_mechanics(index_thumb_distance):
+def click_mechanics(distance):
     global click_status
     
-    if index_thumb_distance < click_distance and click_status == 0:
+    if distance < click_distance and click_status == 0:
         click_status = 1
         pyautogui.mouseDown()
-        # print(F"click dist: {index_thumb_distance}")
+        # print(F"click dist: {distance}")
         return
 
-    elif index_thumb_distance > 100:
+    elif distance > 100:
         click_status = 0
         pyautogui.mouseUp()
     
@@ -68,6 +69,7 @@ async def main():
     global click_status
     global show_stats
     global zero_distance_dragging
+    global rgb_stat
     
     print(f"screen width, height: {screen_width}, {screen_height}")
 
@@ -157,7 +159,7 @@ async def main():
                         
                         index_x = screen_width / frame_width * x
                         index_y = screen_height / frame_height * y
-                        
+            
                     if id == 4: 
                         cv2.circle(frame, center=(x, y), radius=10//global_scale, color=(255, 0, 0)) # smaller red pointer
                         
@@ -171,27 +173,11 @@ async def main():
                         if zero_distance_dragging == True:
                             dragging_threshold = 0
                         else:
-                            dragging_threshold = 130
-                            
-                        if index_thumb_distance > dragging_threshold:
-                            old_cursor_x_1 = previous_mouse_pos_1[0]
-                            old_cursor_y_1 = previous_mouse_pos_1[1]
-                            
-                            old_cursor_x_2 = previous_mouse_pos_2[0]
-                            old_cursor_y_2 = previous_mouse_pos_2[1]
-
-                            calc_cursor_x = (index_x+old_cursor_x_1+old_cursor_x_2)/3
-                            calc_cursor_y = (index_y+old_cursor_y_1+old_cursor_y_2)/3
-                            pyautogui.moveTo(calc_cursor_x, calc_cursor_y)
-
-                            if frame_number % 2 == 0:
-                                previous_mouse_pos_2 = [calc_cursor_x,calc_cursor_y]
-                            else:
-                                previous_mouse_pos_1 = [calc_cursor_x,calc_cursor_y]
-
+                            dragging_threshold = 100
+            
             palm_distance_reciprocal = 1/ptp_distance((lower_palm_x,lower_palm_y),(upper_palm_x,upper_palm_y))
             relative_palm_index_thumb_distance = index_thumb_distance*palm_distance_reciprocal*300
-            
+                                
             if show_window and global_scale == 1:
                 # draws line between index and thumb, adds distance on midpoint of line
                 cv2.line(frame, (int(index_x), int(index_y)), (int(thumb_x), int(thumb_y)), (255, 0, 0), 3)
@@ -203,16 +189,41 @@ async def main():
                 palm_line_midpoint = midpoint(lower_palm_x,lower_palm_y,upper_palm_x,upper_palm_y)
                 cv2.putText(frame, f"Palm Distance Recip.: {round((palm_distance_reciprocal),4)}", palm_line_midpoint, cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2, cv2.LINE_AA)
 
-            # Trigger click mechanics WHEN THERE IS LITTLE ACCELERATION (Holds clicks when dragging)
+            # Aceleration metrics
             average_lower_palm_pos = ((previous_lower_palm_pos2[0] + previous_lower_palm_pos1[0])/2, (previous_lower_palm_pos2[1] + previous_lower_palm_pos1[1])/2)
             relative_lower_palm_aceleration = ptp_distance(average_lower_palm_pos, (lower_palm_x,lower_palm_y)) * palm_distance_reciprocal * 40
             
+            # TRIGGER MOVING MECHANICS AND AVERAGING MECH IF ACELERATION AND DRAGGING THRESHOLD MET
+            if relative_palm_index_thumb_distance > dragging_threshold and relative_lower_palm_aceleration > 0.4:
+                old_cursor_x_1 = previous_mouse_pos_1[0]
+                old_cursor_y_1 = previous_mouse_pos_1[1]
+                
+                old_cursor_x_2 = previous_mouse_pos_2[0]
+                old_cursor_y_2 = previous_mouse_pos_2[1]
+
+                calc_cursor_x = (index_x+old_cursor_x_1+old_cursor_x_2)/3
+                calc_cursor_y = (index_y+old_cursor_y_1+old_cursor_y_2)/3
+                pyautogui.moveTo(calc_cursor_x, calc_cursor_y)
+
+                if frame_number % 2 == 0:
+                    previous_mouse_pos_2 = [calc_cursor_x,calc_cursor_y]
+                else:
+                    previous_mouse_pos_1 = [calc_cursor_x,calc_cursor_y]
+            
+            # Trigger clicking mechanic if aceleration metrics are met
             if relative_lower_palm_aceleration < click_mechanic_aceleration_threshold:
                 click_mechanics(relative_palm_index_thumb_distance)
                 
             if frame_number % 2:
                 relative_lower_palm_aceleration_display = relative_lower_palm_aceleration
+        
+            with open("metrics1.txt", "a") as metricsdoc1:
+                metricsdoc1.write(str(index_thumb_distance) + "\n")
                 
+            with open("metrics2.txt", "a") as metricsdoc2:
+                metricsdoc2.write(str(relative_palm_index_thumb_distance) + "\n")
+                
+
         elif click_status == 1:
             # Stop clicking when hands aren't in frame
             click_mechanics(click_distance+1)
@@ -231,17 +242,19 @@ async def main():
                 text_size = 1
             
             if show_stats == True:
-                color = generate_spectrum_color(color, 3)
+                
+                if rgb_stat == True:
+                    # Spectrum cycles text :)
+                    color = generate_spectrum_color(color, 3)
                 
                 # Status indicators in THIS ORDER: FPS, Index cord, thumb cord, click stat, hand relative distance from camera
                 cv2.putText(frame, f"Index finger: {index_x}, {index_y}", (int(50/global_scale), int(50/global_scale)), cv2.FONT_HERSHEY_SIMPLEX, 1.3/global_scale, color, text_size, cv2.LINE_AA)
                 cv2.putText(frame, f"Thumb finger: {thumb_x}, {thumb_y}", (int(50/global_scale), int(90/global_scale)), cv2.FONT_HERSHEY_SIMPLEX, 1.3/global_scale, color, text_size, cv2.LINE_AA)
                 cv2.putText(frame, f"Click status: {click_status}", (int(50/global_scale), int(130/global_scale)), cv2.FONT_HERSHEY_SIMPLEX, 1.3/global_scale, color, text_size, cv2.LINE_AA)
                 cv2.putText(frame, f"fps: {int(fps)}", (int(50//global_scale), int(180/global_scale)), cv2.FONT_HERSHEY_PLAIN, 3/global_scale, color, text_size, cv2.LINE_AA)
-                cv2.putText(frame, f"Aceleration (relative unit/2s): {round((relative_lower_palm_aceleration_display),2)}", (int(50//global_scale), int(230/global_scale)),
-                            cv2.FONT_HERSHEY_PLAIN, 3/global_scale, color, text_size, cv2.LINE_AA)
-                cv2.putText(frame, f"Zero dist. dragging: {zero_distance_dragging}", (int(50//global_scale), int(280/global_scale)),
-                            cv2.FONT_HERSHEY_PLAIN, 3/global_scale, color, text_size, cv2.LINE_AA)
+                cv2.putText(frame, f"Aceleration (relative unit/2s): {round((relative_lower_palm_aceleration_display),2)}", (int(50//global_scale), int(230/global_scale)),cv2.FONT_HERSHEY_PLAIN, 3/global_scale, color, text_size, cv2.LINE_AA)
+                cv2.putText(frame, f"Zero dist. dragging: {zero_distance_dragging}", (int(50//global_scale), int(280/global_scale)),cv2.FONT_HERSHEY_PLAIN, 3/global_scale, color, text_size, cv2.LINE_AA)
+                cv2.putText(frame, f"RGB: {rgb_stat}", (int(50//global_scale), int(330/global_scale)),cv2.FONT_HERSHEY_PLAIN, 3/global_scale, color, text_size, cv2.LINE_AA)
 
             # final output!!!
             cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
@@ -264,6 +277,9 @@ async def main():
         
         if key == ord('e'):
             zero_distance_dragging = not zero_distance_dragging
+        
+        if key == ord('r'):
+            rgb_stat = not rgb_stat
             
         elif cv2.getWindowProperty(WINDOW_NAME,cv2.WND_PROP_VISIBLE) < 1:        
             break    
