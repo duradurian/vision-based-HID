82e4a13c50d97b80814bcb2a7017e4533bff3f22
duradurian <113811889+duradurian@users.noreply.github.com>
2024-01-07 00:54:06 -0500

    yes

--- apriltagtest.py
+++ apriltagtest.py
@@ -1,12 +1,6 @@
 import cv2 as cv2
-import math
 
-def main():
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_16h5)
-    parameters =  cv2.aruco.DetectorParameters()
-    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-    
-    def aruco_display(corners, ids, image):
+def aruco_display(corners, ids, image):
         if len(corners) > 0:
     
             ids = ids.flatten()
@@ -27,13 +21,26 @@ def main():
                 slope_top = (topRight[1] - topLeft[1]) / (topRight[0] - topLeft[0])
                 slope_bottom = (bottomRight[1] - bottomLeft[1]) / (bottomRight[0] - bottomLeft[0])
                 
+                tag_midpoint = (int((topRight[0] + topLeft[0] + bottomLeft[0] + bottomRight[0])/4), int((topRight[1] + topLeft[1] + bottomLeft[1] + bottomRight[1])/4))
+                cv2.circle(image, tag_midpoint, 10, (255,255,0), 2)
+                
+                tag_in_aim = (tag_midpoint[0] - 1280/2)**2 + (tag_midpoint[1] - 720/2)**2 < 50**2
+                
+                if tag_in_aim:
+                    circle_color = (0,255,0)
+                else:
+                    circle_color = (0,0,255)
+                
+                cv2.circle(image, (int(1280/2), int(720/2)), 50, circle_color, 2)
+                
+                
                 if abs(slope_top) < 0.03:
                     top_color = (0, 255, 0)
                     
                 if abs(slope_bottom) < 0.03:
                     bottom_color = (0, 255, 0)
                     
-                if abs(slope_bottom) < 0.03 and abs(slope_top) < 0.03:
+                if abs(slope_bottom) < 0.03 and abs(slope_top) < 0.03 and tag_in_aim:
                     cv2.putText(image, "READY TO SHOOT", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 1, cv2.LINE_AA)
                     
                 # PUT SLOPE NUMBERS ON MIDPOINTS OF THE LINES
@@ -42,8 +49,6 @@ def main():
                 cv2.putText(image, str(round(slope_top, 3)), midpoint_top, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 1, cv2.LINE_AA)
                 cv2.putText(image, str(round(slope_bottom, 3)), midpoint_bottom, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (2055,0,255), 1, cv2.LINE_AA)
                 
-                deg = (math.atan(slope_top))*(180/math.pi)
-                print(deg)
                 
                 cv2.line(image, topLeft, topRight, top_color, 3)
                 cv2.line(image, topRight, bottomRight, (255, 0, 0), 3)
@@ -52,8 +57,16 @@ def main():
                 
         return image
     
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+def main():
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_16h5)
+    parameters =  cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
     
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.CAP_PROP_FPS, 60)
+        
     while cap.isOpened():
     
         ret, img = cap.read()
