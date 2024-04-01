2abec9b01ac368f925d1bcb033001489a0174971
duradurian <113811889+duradurian@users.noreply.github.com>
2024-04-01 02:19:30 -0400

    Update headmouse.py

--- headmouse.py
+++ headmouse.py
@@ -7,6 +7,9 @@ import asyncio
 import cv2
 import mediapipe as mp
 import pyautogui
+import math
+import gui
+import time
 
 # Set pyautogui settings
 pyautogui.PAUSE = 0
@@ -14,7 +17,7 @@ pyautogui.FAILSAFE = False
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
-AVERAGING_TIMES = 30 # recommended 30
+AVERAGING_TIMES = 20 # recommended 30
 average_pos = []
 
 for i in range(AVERAGING_TIMES):
@@ -41,7 +44,12 @@ async def main():
     face_mesh = mp_face.FaceMesh(max_num_faces=1)
     drawing_utils = mp.solutions.drawing_utils
 
+    # Class instances
+    fps_instance = gui.FPS(start_time = time.time())
+    color = (120, 255, 255)
+    
     while True:
+        fps_instance.new_frame()
         ret, frame = cap.read()
 
         if not ret:
@@ -94,8 +102,10 @@ async def main():
                     if id == 199:
                         cv2.circle(frame, center=(x, y), radius=10, color=(0, 255, 0)) # green pointer
                         bottom_center = [SCREEN_WIDTH / frame_width * x, SCREEN_HEIGHT / frame_height * y]
+                        
+                # prevents lag in case of 2 faces (redundant but yea)    
+                break
                 
-            
             # x position logic
             delta_center_bottom_left = center[0] - bottom_left[0]
             delta_center_bottom_right = bottom_right[0] - center[0]
@@ -106,7 +116,7 @@ async def main():
             delta_center_top_left = center[1] - top_left[1]
             delta_center_top_right = center[1] - top_right[1]
             average_top_center = (delta_center_top_left+delta_center_top_right)/2
-            print(average_top_center)
+            # print(average_top_center)
             
             # movement
             cursor_pos = (delta_center_bottom_left-delta_center_bottom_right+68)*15, (average_top_center-57)*50
@@ -123,18 +133,34 @@ async def main():
             average_x /= AVERAGING_TIMES
             average_y /= AVERAGING_TIMES
             
+            acceleration = math.sqrt((average_pos[-1][0] - average_pos[-2][0])**2 + (average_pos[-3][1] - average_pos[-3][1])**2)
+            print(acceleration)
+            
             # print(average_x, average_y)  
-                          
-            pyautogui.moveTo(average_x, average_y)
+            if acceleration > 5:
+                pyautogui.moveTo(average_x, average_y)
 
         # final output!!!
+        color = gui.generate_spectrum_color(color, 3)
+        fps, fps_average = fps_instance.calc_fps()
+        display_list = [
+            f"EARLY VERSION",
+            f"FPS, FPS AVERAGE: {round(fps, 1), round(fps_average, 1)}"
+            ]
+    
+        for text_index in range(len(display_list)):
+            cv2.putText(frame, display_list[text_index], (int(50), int(text_index*50+50)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, color, 2, cv2.LINE_AA)
+            
         cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("test", int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2))
         cv2.imshow("test", frame)
         
         key = cv2.waitKey(1) & 0xFF
         
         if key == ord('q'):
             break
+        
+        fps_instance.new_frame(add_frame_count = True)
        
             
     cap.release()
